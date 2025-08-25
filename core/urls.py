9307afae23088c2bfff4f9d14a4493daaf441991
lgluiz1 <legalhints@gmail.com>
2025-08-25 10:26:55 -0300

    redirecionar para docs

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -12,6 +13,14 @@ from drf_spectacular.views import SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # Redireciona raiz para /api/docs
+    path('', lambda request: redirect('/api/docs/')),
+
+
+    # JWT auth
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
     # JWT auth
     #path('api/token/', obtain_auth_token, name='api_token_auth'),
