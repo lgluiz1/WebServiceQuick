a21e9eeb5595364a283e85af6ffd42604a32135a
lgluiz1 <legalhints@gmail.com>
2025-09-29 14:24:40 -0300

    ADMIN DJANGO

--- webhooks/admin.py
+++ webhooks/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import FretesWebhook, NfeWebhook
+from .models import FretesWebhook, NfeWebhook, ManifestoWebhook
 
 admin.site.register(FretesWebhook)
+@admin.register(ManifestoWebhook)
+class ManifestoWebhookAdmin(admin.ModelAdmin):
+    list_display = ('manifesto_numero', 'recebido_em', 'processado', 'processado_em', 'erro')
+    list_filter = ('processado', 'recebido_em', 'processado_em')
+    search_fields = ('manifesto_numero',)
 @admin.register(NfeWebhook)
 class NfeWebhookAdmin(admin.ModelAdmin):
     list_display = ('numero_nfe', 'recebido_em', 'processado', 'erro')
