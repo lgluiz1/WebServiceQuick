efa54ac2bfaead12aac213137fe8c91acb2edb02
lgluiz1 <legalhints@gmail.com>
2025-09-30 15:12:05 -0300

    apps.py

--- webhooks/apps.py
+++ webhooks/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class WebhooksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'webhooks'
+
+    def ready(self):
+        import webhooks.signals  # Importa os sinais para conectar os handlers
