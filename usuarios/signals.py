c82fe5499ac6d1d9d7028a294f0df57ba26696a9
lgluiz1 <legalhints@gmail.com>
2025-10-01 16:24:40 -0300

    usuarios

--- usuarios/signals.py
+++ usuarios/signals.py
@@ -3,25 +3,27 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import Usuario
+import threading
 
 @receiver(post_save, sender=Usuario)
 def enviar_email_ativacao(sender, instance, created, **kwargs):
     if created:
-        token = instance.gerar_token_ativacao()
-        link_ativacao = f"{settings.SITE_URL}/ativar-conta/{token}/"
+        def _send():
+            token = instance.gerar_token_ativacao()
+            link_ativacao = f"{settings.SITE_URL}/ativar-conta/{token}/"
+            assunto = "Ative sua conta no sistema"
+            mensagem = (
+                f"Olá {instance.nome},\n\n"
+                f"Seu cadastro foi realizado. Para ativar sua conta, clique no link abaixo:\n"
+                f"{link_ativacao}\n\n"
+                "Se você não fez esse cadastro, ignore este e-mail."
+            )
+            send_mail(
+                assunto,
+                mensagem,
+                settings.DEFAULT_FROM_EMAIL,
+                [instance.email],
+                fail_silently=False,
+            )
 
-        assunto = "Ative sua conta no sistema"
-        mensagem = (
-            f"Olá {instance.nome},\n\n"
-            f"Seu cadastro foi realizado. Para ativar sua conta, clique no link abaixo:\n"
-            f"{link_ativacao}\n\n"
-            "Se você não fez esse cadastro, ignore este e-mail."
-        )
-
-        send_mail(
-            assunto,
-            mensagem,
-            settings.DEFAULT_FROM_EMAIL,
-            [instance.email],
-            fail_silently=False,
-        )
+        threading.Thread(target=_send).start()
