d367129dea259c746c395a20fadc46e017804ac5
lgluiz1 <legalhints@gmail.com>
2025-10-01 16:12:26 -0300

    usuarios

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Usuario
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('email', 'nome', 'perfil', 'telefone', 'cpf', 'filial' )
+    search_fields = ('email', 'nome', 'cpf', 'telefone')
+    list_filter = ('perfil', 'filial')
