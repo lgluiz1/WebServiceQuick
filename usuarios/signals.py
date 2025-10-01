ece94613360d146f1e43981bd1dc523a6773613f
lgluiz1 <legalhints@gmail.com>
2025-10-01 17:03:36 -0300

    usuarios

--- usuarios/signals.py
+++ usuarios/signals.py
@@ -3,28 +3,17 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import Usuario
-import threading
 
 @receiver(post_save, sender=Usuario)
 def enviar_email_ativacao(sender, instance, **kwargs):
-    # se o usuário ainda não tiver token de ativação ou não estiver ativo
-    if not hasattr(instance, "token_ativacao") or not instance.ativo:
-        token = instance.gerar_token_ativacao()
-        link_ativacao = f"{settings.SITE_URL}/ativar-conta/{token}/"
+    def send_activation_email(sender, instance, created):
 
-        assunto = "Ative sua conta no sistema"
-        mensagem = (
-            f"Olá {instance.nome},\n\n"
-            f"Seu cadastro foi realizado. Para ativar sua conta, clique no link abaixo:\n"
-            f"{link_ativacao}\n\n"
-            "Se você não fez esse cadastro, ignore este e-mail."
-        )
+        if created :
+            send_mail(
+                'Bem-vindo ao Sistema',
+                f'Seu usuário foi criado com sucesso. Seu email de login é: {instance.email}',
+                settings.DEFAULT_FROM_EMAIL,
+                [instance.email],
+                fail_silently=False,
+            )
 
-        send_mail(
-            assunto,
-            mensagem,
-            settings.DEFAULT_FROM_EMAIL,
-            [instance.email],
-            fail_silently=False,
-        )
-        
