caad39fc5002d4099ac41750e80613689a5561bf
lgluiz1 <legalhints@gmail.com>
2025-10-01 12:21:01 -0300

    fretess

--- fretes/admin.py
+++ fretes/admin.py
@@ -1,2 +1,9 @@
 # fretes/admin.py
 from django.contrib import admin
+from .models import Frete
+
+@admin.register(Frete)
+class FreteAdmin(admin.ModelAdmin):
+    list_display = ('frete_id', 'chave', 'numero', 'serie', 'valor', 'data_criacao', 'status')
+    list_filter = ('status', 'data_criacao')
+    search_fields = ('frete_id', 'chave')
