347bb08882c0a90626df4d31c746a56f1e0aae2e
lgluiz1 <legalhints@gmail.com>
2025-10-01 16:15:49 -0300

    usuarios

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -5,4 +5,4 @@ from .models import Usuario
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ('email', 'nome', 'tipo_perfil', 'telefone', 'cpf', 'filial' )
     search_fields = ('email', 'nome', 'cpf', 'telefone')
-    list_filter = ('perfil', 'filial')
+    list_filter = ('tipo_perfil', 'filial')
