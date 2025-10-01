538b97e26267d4af644d0a08b3db0254aad8d2f6
lgluiz1 <legalhints@gmail.com>
2025-10-01 13:15:06 -0300

    filial

--- filial/models.py
+++ filial/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 class Filial(models.Model):
     documento = models.CharField(max_length=14, unique=True)
-    ie = models.CharField(max_length=14)
-    nome = models.CharField(max_length=100)
-    endereco = models.CharField(max_length=100)
-    bairro = models.CharField(max_length=100)
-    cidade = models.CharField(max_length=100)
-    uf = models.CharField(max_length=2)
-    cep = models.CharField(max_length=9)
-    telefone = models.CharField(max_length=14)
-    email = models.EmailField()
-    ultima_atualizacao = models.DateTimeField(auto_now=True)
+    ie = models.CharField(max_length=14,blank=True, null=True)
+    nome = models.CharField(max_length=100,blank=True, null=True)
+    endereco = models.CharField(max_length=100,blank=True, null=True)
+    bairro = models.CharField(max_length=100,blank=True, null=True)
+    cidade = models.CharField(max_length=100,blank=True, null=True)
+    uf = models.CharField(max_length=2,blank=True, null=True)
+    cep = models.CharField(max_length=9,blank=True, null=True)
+    telefone = models.CharField(max_length=14,blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    ultima_atualizacao = models.DateTimeField(auto_now=True,blank=True, null=True)
 
     def __str__(self):
         return f"{self.nome} "
