4d27e4d8b01e6e6bd49c1428ba50a2f77bf42ff2
lgluiz1 <legalhints@gmail.com>
2025-10-01 14:48:17 -0300

    fretess

--- fretes/admin.py
+++ fretes/admin.py
@@ -4,6 +4,6 @@ from .models import Frete
 
 @admin.register(Frete)
 class FreteAdmin(admin.ModelAdmin):
-    list_display = ('frete_id', 'chave', 'numero', 'serie', 'valor_total_produtos', 'data_emissao', 'status')
+    list_display = ('frete_id', 'chave', 'numero', 'serie', 'valor_total_produtos', 'data_emissao', 'status', 'atualizado_em')
     list_filter = ('status', 'data_emissao')
     search_fields = ('frete_id', 'chave')
